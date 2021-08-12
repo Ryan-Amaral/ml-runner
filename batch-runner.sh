7a90beebd572496d65b83220b57c71f8cd1812c3
Ryan-Amaral <ryan_amaral@live.com>
2021-08-12 13:22:29 -0300

    updated batch script

--- batch-runner.sh
+++ batch-runner.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# argument $1: relative path to the mlrunner experiment script
-
-for i in {1..20}; do
+for i in {1..4}; do
     tmux new-session -d -s 2dbiped-gp${i}
-    tmux send-keys -t 2dbiped-gp${i}:0 "conda activate 2dbiped-gp" Enter
-    tmux send-keys -t 2dbiped-gp${i}:0 "python $1 --instance ${i} --end_generation 500 --episodes 5 --environment BipedalWalker-v3 --frames 1600 --processes 2 --trainer_checkpoint 100 --init_team_pop 360 --gap 0.5 --init_max_act_prog_size 128 --inst_del_prob 0.5 --inst_add_prob 0.5 --inst_swp_prob 0.5 --inst_mut_prob 0.5 --elitist --ops robo --rampancy 5,5,5" Enter
+    tmux send-keys -t 2dbiped-gp${i}:0 "conda activate pyton" Enter
+    tmux send-keys -t 2dbiped-gp${i}:0 "python -c 'from mlrunner.experiment import run_experiment; run_experiment(instance=${i}, processes=2)'" Enter
 done
