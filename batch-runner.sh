e4454a24c9ba91ccc331e05c2d9d8bc794c272ff
Ryan-Amaral <ryan_amaral@live.com>
2021-08-11 21:15:12 -0300

    ready to run

--- batch-runner.sh
+++ batch-runner.sh
@@ -1,3 +1,9 @@
-# something like this
-tmux new-session -d -s mysesh
-tmux send-keys -t mysesh:0 "python script.py" Enter
+#!/bin/bash
+
+# argument $1: relative path to the mlrunner experiment script
+
+for i in {1..20}; do
+    tmux new-session -d -s 2dbiped-gp${i}
+    tmux send-keys -t 2dbiped-gp${i}:0 "conda activate 2dbiped-gp" Enter
+    tmux send-keys -t 2dbiped-gp${i}:0 "python $1 --instance ${i} --end_generation 500 --episodes 5 --environment BipedalWalker-v3 --frames 1600 --processes 2 --trainer_checkpoint 100 --init_team_pop 360 --gap 0.5 --init_max_act_prog_size 128 --inst_del_prob 0.5 --inst_add_prob 0.5 --inst_swp_prob 0.5 --inst_mut_prob 0.5 --elitist --ops robo --rampancy 5,5,5" Enter
+done
